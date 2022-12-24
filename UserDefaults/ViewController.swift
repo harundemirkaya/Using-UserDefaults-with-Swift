48fbc32570a151088a82b13a7f89134d3ba5b311
harundemirkaya <harundmrk@gmail.com>
2022-12-25 02:32:00 +0300

    Final Commit

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var noteTextField: UITextField!
+    
+    @IBOutlet weak var timeTextField: UITextField!
+    
+    @IBOutlet weak var noteLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let savedNote =  UserDefaults.standard.object(forKey: "note")
+        let savedTime = UserDefaults.standard.object(forKey: "time")
+        
+        if let incomeNote = savedNote as? String{
+            noteLabel.text = "Your Note: \(incomeNote)"
+        }
+        if let incomeTime = savedTime as? String{
+            timeLabel.text = "When: \(incomeTime)"
+        }
+        
     }
 
-
+    
+    @IBAction func btnSave(_ sender: Any) {
+        UserDefaults.standard.set(noteTextField.text!, forKey: "note")
+        UserDefaults.standard.set(timeTextField.text!, forKey: "time")
+        noteLabel.text = "Your Note: \(noteTextField.text!)"
+        timeLabel.text = "When: \(timeTextField.text!)"
+    }
+    
+    @IBAction func btnDelete(_ sender: Any) {
+        let savedNote =  UserDefaults.standard.object(forKey: "note")
+        let savedTime = UserDefaults.standard.object(forKey: "time")
+        
+        if (savedNote as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "note")
+            noteLabel.text = "Your Note: "
+        }
+        
+        if (savedTime as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "time")
+            timeLabel.text = "When: "
+        }
+    }
 }
 
